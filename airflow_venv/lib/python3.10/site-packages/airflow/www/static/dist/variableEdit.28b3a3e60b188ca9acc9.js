fce296a670926fa16d9c836a672d4a43e25a3887
saifulAchmad <saiful.achmad.fajar@gmail.com>
2024-08-21 07:37:45 +0700

    first commit

---
+++ airflow_venv/lib/python3.10/site-packages/airflow/www/static/dist/variableEdit.28b3a3e60b188ca9acc9.js
@@ -0,0 +1,2 @@
+/*! For license information please see variableEdit.28b3a3e60b188ca9acc9.js.LICENSE.txt */
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.Airflow=t():(e.Airflow=e.Airflow||{},e.Airflow.variableEdit=t())}(self,(function(){return function(){const e=document.getElementById("val"),t=Math.min(.5*window.innerHeight,e.scrollHeight);return e.style.height="".concat(t,"px"),{}}()}));
---
+++ dags/daily_report.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+# Default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 8, 21),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
+}
+
+# Define the DAG
+dag = DAG(
+    'simple_dag',
+    default_args=default_args,
+    description='A simple DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+# Define the tasks
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
+
+t2 = BashOperator(
+    task_id='sleep',
+    bash_command='sleep 5',
+    dag=dag,
+)
+
+t3 = BashOperator(
+    task_id='print_hello',
+    bash_command='echo "Hello World"',
+    dag=dag,
+)
+
+# Set the task dependencies
+t1 >> t2 >> t3
